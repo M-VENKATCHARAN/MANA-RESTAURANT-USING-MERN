185a1a94594503f05cfa711b0b897dab5daee2a6
MVC <charan29062005@gmail.com>
2025-04-17 17:08:39 +0530

    Mana

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">MANA RESTAURANT</div>
+          <div className="right">
+            <p>ELECTRONIC CITY</p>
+            <p>Opening Hours: 9:00 AM - 10:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>© 2024 MANA RESTAURANT</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
